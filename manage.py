0cd3129437fc772657c1d1c592d694361fb7f549
liwanlei <leileili126@163.com>
2017-08-16 17:44:01 +0800

    开发进度汇总、

--- manage.py
+++ manage.py
@@ -5,8 +5,5 @@
 @time: 2017/7/13 16:39g
 """
 from  app import  app
-from flask_admin import Admin,AdminIndexView
-from flask_admin.contrib.sqla import ModelView
-from app.models import User,Interface,InterfaceTest,TestResult,db
 if __name__ == '__main__':
     app.run()
