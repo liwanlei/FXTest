42ad78a1b1407d61383472cdeafe1961bc1ca890
liwanlei <leileili126@163.com>
2017-08-17 13:27:36 +0800

    合并后试运行，

--- app/form.py
+++ app/form.py
@@ -15,39 +15,36 @@ work_list=Work.query.all()
 for i in range(len(work_list)):
     choice_list.append((work_list[i].id,work_list[i].name))
 class LoginFrom(Form):
-    username=StringField(u'用户名',[validators.Length(min=4, max=16,message=u'用户名长度在4-16位'),validators.DataRequired(message=u'用户名不能为空')],render_kw={'placeholder':u'请输入用户名'})
-    password=PasswordField(u'密码',[validators.length(min=8,max=16,message=u'密码长度8-16位'),validators.DataRequired(message=u'密码不能为空')],render_kw={'placeholder':u'请输入密码'})
+    username=StringField(u'用户名',[validators.Length(min=4, max=16,message=u'用户名长度在4-16位'),validators.DataRequired()],render_kw={'placeholder':u'请输入用户名'})
+    password=PasswordField(u'密码',[validators.length(min=8,max=16,message=u'密码长度8-16位'),validators.DataRequired()],render_kw={'placeholder':u'请输入密码'})
 class RegFrom(Form):
-	username = StringField(u'注册用户名', [validators.Length(min=4, max=16, message=u'用户名长度在4-16位'), validators.DataRequired(message=u'用户名不能为空')],render_kw={'placeholder': u'请输入用户名'})
-	password = PasswordField(u'注册密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'密码不能为空')],render_kw={'placeholder': u'请输入密码'})
-	#se_password = PasswordField(u'再次输入密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'密码不能为空')],
-	username = StringField(u'注册用户名', [validators.Length(min=4, max=16, message=u'用户名长度在4-16位'), validators.DataRequired(message=u'请输入用户名')],render_kw={'placeholder': u'请输入用户名'})
-	password = PasswordField(u'注册密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'请输入密码')],render_kw={'placeholder': u'请输入密码'})
-	se_password =PasswordField(u'再次输入密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'请输入确认密码')],render_kw={'placeholder': u'请输入确认密码'})
-	email=StringField(u'输入注册邮箱', [ validators.DataRequired(message=u'注册邮箱不能为空')],
-	email=StringField(u'输入注册邮箱', [ validators.DataRequired(message=u'请输入邮箱'),Email(message=u'邮箱格式不对')],
-                         render_kw={'placeholder': u'请输入邮箱'})
-	work=SelectField(u'选择职位',choices=choice_list,coerce=int,validators=[validators.DataRequired(message=u"项目名称不能为空")])
+    username = StringField(u'注册用户名', [validators.Length(min=4, max=16, message=u'用户名长度在4-16位'), validators.DataRequired(message=u'请输入用户名')],render_kw={'placeholder': u'请输入用户名'})
+    password = PasswordField(u'注册密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'请输入密码')],render_kw={'placeholder': u'请输入密码'})
+    se_password = PasswordField(u'再次输入密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'请输入确认密码')],
+                             render_kw={'placeholder': u'请输入密码'})
+    email=StringField(u'输入注册邮箱', [ validators.DataRequired(message=u'请输入邮箱'),Email(message=u'邮箱格式不对')],
+                           render_kw={'placeholder': u'请输入邮箱'})
+    work=SelectField(u'选择职位',choices=choice_list,coerce=int,validators=[validators.DataRequired(message=u"项目名称不能为空")])
 class XugaiFrom(Form):
-	password = PasswordField(u'密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'原密码不能为空')],
+    password = PasswordField(u'密码', [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired()],
                              render_kw={'placeholder': u'请输入原密码'})
-	xiu_password = PasswordField(u'密码',
-                                [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'密码不能为空')],
+    xiu_password = PasswordField(u'密码',
+                                [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired()],
                                 render_kw={'placeholder': u'请输入新密码'})
-	xiu_password_se = PasswordField(u'密码',
-                                 [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired(message=u'密码不能为空')],
+    xiu_password_se = PasswordField(u'密码',
+                                 [validators.length(min=8, max=16, message=u'密码长度8-16位'), validators.DataRequired()],
                                  render_kw={'placeholder': u'请再次输入密码'})
 class InterForm(Form):
-	project_name=StringField(u'项目名字', [validators.DataRequired(message=u'项目名不能为空')],render_kw={'placeholder': u'请输入接口所属项目名称'})
-	model_name=StringField(u'模块名字', [validators.DataRequired(message=u'模块名不能为空')],render_kw={'placeholder': u'请输入接口所属模块名称'})
-	interface_name=StringField(u'接口名字', [validators.DataRequired(message=u'接口名不能为空')],render_kw={'placeholder': u'请输入接口名称'})
-	interface_url=StringField(u'接口url', [validators.DataRequired(message=u'接口url不能为空')],render_kw={'placeholder': u'请输入接口url'})
-	interface_meth=StringField(u'请求方式', [validators.DataRequired(message=u'请求方式不能为空')],render_kw={'placeholder': u'请输入接口请求方式'})
-	interface_par = StringField(u'请求示例', [validators.DataRequired(message=u'请求示例不能为空')], render_kw={'placeholder': u'请输入接口参数示例'})
-	interface_bas= StringField(u'请求返回示例', [validators.DataRequired(message=u'返回示例不能为空')], render_kw={'placeholder': u'请输入接口返回示例'})
+    project_name=StringField(u'项目名字', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口所属项目名称'})
+    model_name=StringField(u'模块名字', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口所属模块名称'})
+    interface_name=StringField(u'接口名字', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口名称'})
+    interface_url=StringField(u'接口url', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口url'})
+    interface_meth=StringField(u'请求方式', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口请求方式'})
+    interface_par = StringField(u'请求示例', [validators.DataRequired()], render_kw={'placeholder': u'请输入接口参数示例'})
+    interface_bas= StringField(u'请求返回示例', [validators.DataRequired()], render_kw={'placeholder': u'请输入接口返回示例'})
 class Interface_yong_Form(Form):
-	yongli_name=StringField(u'项目', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口项目名称'})
-	model_name=StringField(u'模块', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口模块名称'})
+    yongli_name=StringField(u'项目', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口项目名称'})
+    model_name=StringField(u'模块', [validators.DataRequired()],render_kw={'placeholder': u'请输入接口模块名称'})
     interface_name = StringField(u'接口名字', [validators.DataRequired()], render_kw={'placeholder': u'请输入接口名称'})
     interface_url = StringField(u'接口url', [validators.DataRequired()], render_kw={'placeholder': u'请输入接口url'})
     interface_meth = StringField(u'请求方式', [validators.DataRequired()], render_kw={'placeholder': u'请输入接口请求方式'})
@@ -59,3 +56,4 @@ class Set_email_Form(Form):
     password=StringField(u'请输入安全密码：', [validators.DataRequired(message='请输入安全密码')],render_kw={'placeholder': u'请输入邮箱安全密码'})
     stmp_email=StringField(u'请输入stmp地址：', [validators.DataRequired(message='请输入stmp地址：')],render_kw={'placeholder': u'请输入stmp地址：'})
     port=StringField(u'请输入stmp的端口号：', [validators.DataRequired(message='请输入stmp的端口号：')],render_kw={'placeholder': u'请输入stmp的端口号：'})
+    
