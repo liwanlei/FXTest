2ea7c5086c6cfc7b70e82dc1fef8f7078006a43a
liwanlei <leileili126@163.com>
2018-08-27 16:46:16 +0800

    增加修改密码，去掉邮箱设置

--- app/users/views.py
+++ app/users/views.py
@@ -4,13 +4,12 @@
 # @Time    : 2017/12/7 12:25
 from flask import  Blueprint
 user = Blueprint('user', __name__)
-from  flask import  redirect,request,render_template,\
+from  flask import  redirect,request,\
     session,url_for,flash,jsonify
 from  app.models import *
-from app.form import  *
-from flask.views import MethodView,View
+from flask.views import View,MethodView
 from common.decorators import chckuserpermisson
-from flask_login import current_user,login_required
+from flask_login import login_required
 from config import OneAdminCount
 class SetadView(View):#设置管理员
     methods=['GET','POST']
@@ -142,155 +141,15 @@ class RedpassView(View):#重置密码
             return redirect(url_for('home.adminuser'))
         flash(u'自己不能重置自己的密码')
         return redirect(url_for('home.adminuser'))
-class Set_emaiView(MethodView):#设置发送测试报告的邮件的
-    @login_required
-    def get(self):
-        email_report=EmailReport.query.filter_by(email_re_user_id=current_user.id,status=False).all()
-        if len(email_report)<=0:
-            return render_template('home/set_send.html', errmessage=u'您还没有设置发送测试报告邮件')
-        return render_template('home/set_send.html', email_reports=email_report)
-class Add_emaiView(MethodView):#添加邮件
-    @login_required
-    def get(self):
-        form=Set_email_Form()
-        return render_template('add/add_emali.html', form=form)
+class ChangePassword(MethodView):
     @login_required
     def post(self):
-        form=Set_email_Form()
-        if form.validate_on_submit():
-            email=request.form.get('send_email')
-            password=request.form.get('password')
-            shi_f=request.form.get('checkbox')
-            resv_email=request.form.get('email')
-            resv_email=(str(resv_email).split(','))
-            port=request.form.get('port')
-            stmp_email=request.form.get('stmp_email')
-            if email =='' or password=='' or resv_email=='' or port=='' or stmp_email =='':
-                flash(u'请准确填写信息')
-                return render_template('add/add_emali.html', form=form)
-            user_id=current_user.id
-            if shi_f =='on':
-                shi_f=True
-                user_is=EmailReport.query.filter_by(email_re_user_id=user_id,default_set=True).first()
-                if user_is:
-                    flash(u'只能有一个为默认设置')
-                    return render_template('add/add_emali.html', form=form)
-                email_new=EmailReport(email_re_user_id=int(user_id),send_email=str(email),send_email_password=str(password),to_email=str(resv_email),default_set=True,port=int(port),stmp_email=str(stmp_email))
-                db.session.add(email_new)
-                db.session.commit()
-                flash(u'成功设置一个默认配置')
-                return redirect(url_for('user.setting'))
-            email_new=EmailReport(email_re_user_id=int(user_id),send_email=str(email),send_email_password=str(password),to_email=str(resv_email))
-            db.session.add(email_new)
-            try:
-                db.session.commit()
-                flash(u'成功设置一个配置')
-                return redirect(url_for('user.setting'))
-            except:
-                db.session.rollback()
-                flash(u'配置过程出现了异军突起')
-                return redirect(url_for('user.setting'))
-        return render_template('add/add_emali.html', form=form)
-class DeleteView(View):#删除邮件
-    methods=['GET','POST']
-    @login_required
-    def dispatch_request(self,id):
-        email_re=EmailReport.query.filter_by(id=id,status=False).first()
-        user_id=current_user.id
-        if email_re.email_re_user_id==int(user_id):
-            email_re.status=True
-            try:
-                db.session.commit()
-                flash(u'删除成功')
-                return redirect(url_for('user.setting'))
-            except Exception as e:
-                db.session.rollback()
-                flash(u'删除失败,原因：%s'%e)
-                return  redirect(url_for('user.setting'))
-        flash(u'您没有权限删除')
-        return redirect(url_for('user.setting'))
-class EditemailView(MethodView):#编辑邮件
-    @login_required
-    def get(self,id):
-        emai=EmailReport.query.filter_by(id=id).first()
-        return render_template('edit/edit_emali.html', emai=emai)
-    @login_required
-    def post(self,id):
-        emai=EmailReport.query.filter_by(id=id).first()
-        email=request.form.get('send_email')
-        password=request.form.get('password')
-        shi_f=request.form.get('checkbox')
-        resv_email=request.form.get('email')
-        resv_email=(str(resv_email).split(','))
-        stmp_em=request.form.get('stmp')
-        port=request.form.get('port')
-        if email =='' or password=='' or resv_email=='' or stmp_em =='' or port =='':
-            flash(u'请准确填写信息')
-            return render_template('edit/edit_emali.html', emai=emai)
-        user_id=current_user.id
-        user_is = EmailReport.query.filter_by(email_re_user_id=user_id, default_set=True).first()
-        if user_is:
-            flash(u'只能有一个为默认设置')
-            return render_template('edit/edit_emali.html', emai=emai)
-        if shi_f =='on':
-            default_set = True
-        else:
-            default_set=False
-        emai.email_re_user_id=int(user_id)
-        emai.send_email=str(email)
-        emai.send_email_password=str(password)
-        emai.to_email=str(resv_email)
-        emai.stmp_email=str(stmp_em)
-        emai.port=int(port)
-        emai.default_set=default_set
+        password=request.data.decode('utf-8')
+        user = User.query.filter_by(username=session.get('username')).first()
+        user.set_password(password)
         try:
             db.session.commit()
-            flash(u'编辑成功')
-            return redirect(url_for('user.setting'))
-        except:
+            return  jsonify({'code':1,'data':'修改密码成功'})
+        except Exception as e:
             db.session.rollback()
-            flash(u'编辑过程中出现了小抑菌')
-            return redirect(url_for('user.setting'))
-class QuzhiMoView(MethodView):#取消默认
-    @login_required
-    def get(self,id):
-        del_em=EmailReport.query.filter_by(id=id,status=False).first()
-        if del_em:
-            if int(current_user.id)==del_em.email_re_user_id:
-                del_e=EmailReport.query.filter_by(email_re_user_id=int(current_user.id),default_set=True,status=True).all()
-                del_e.default_set=False
-                try:
-                    db.session.commit()
-                    flash(u'取消默认成功')
-                    return redirect(url_for('user.setting'))
-                except Exception as e:
-                    db.session.rollback()
-                    flash(u'取消默认失败')
-                    return  redirect(url_for('user.setting'))
-            flash(u'您没有权限来取消')
-            return redirect(url_for('user.setting'))
-        flash(u'你要取消的默认不存在')
-        return redirect(url_for('user.setting'))
-class ShezhiMoView(MethodView):#设置默认
-    @login_required
-    def get(self,id):
-        shezi_em=EmailReport.query.filter_by(id=id).first()
-        if shezi_em:
-            if int(current_user.id)==shezi_em.email_re_user_id:
-                del_e=EmailReport.query.filter_by(email_re_user_id=int(current_user.id),default_set=True,status=False).all()
-                if len(del_e)>0:
-                    flash(u'一个账户只能有一个默认设置')
-                    return redirect(url_for('user.setting'))
-                shezi_em.default_set=True
-                try:
-                    db.session.commit()
-                    flash(u'设置默认成功')
-                    return redirect(url_for('user.setting'))
-                except Exception as e:
-                    db.session.rollback()
-                    flash(u'设置默认失败，原因：%s'%e)
-                    return redirect(url_for('user.setting'))
-            flash(u'您没有权限来设置')
-            return redirect(url_for('user.setting'))
-        flash(u'你要设置的默认邮箱配置不存在')
-        return redirect(url_for('user.setting'))
+            return jsonify({'code': 2, 'data': '修改密码失败'})
